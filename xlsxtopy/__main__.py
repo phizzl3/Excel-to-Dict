5a54fc3641cff86ad26fcca938b1135e190a0f6f
Brandon Phillips <phizzl3@gmail.com>
2021-02-13 22:01:53 -0600

    Class-Update (#3)

--- xlsxtopy/__main__.py
+++ xlsxtopy/__main__.py
@@ -17,84 +17,104 @@ DLS = Path().home() / "Downloads"
 TODAY = datetime.today()
 
 
-def get_header_row() -> int:
-    """
-    Gets header row number via user input.
+class PyGen(Xlsx):
 
-    Returns:
-        int: Row number containing headers.
-    """
-    try:
-        while True:
-            hr = input("\n Which Row Number contains the headers?: ")
-            if hr.isdigit() and hr != '0':
-                return int(hr)
-    except Exception as e:
-        input(f"\n Error on get_header_row: {e}")
-        exit()
+    def get_header_row(self) -> object:
+        """
+        Gets header row number via user input.
+        Adds row number to .headerrow attribute.
 
+        Returns:
+            self: PyGen Object.
+        """
+        try:
+            while True:
+                hr = input("\n Which Row Number contains the headers?: ")
+                if hr.isdigit() and hr != '0':
+                    self.headerrow = int(hr)
+                    return self
 
-def get_key_col() -> str:
-    """
-    Gets column letter containing data to be used as keys in generated dictionary.
-
-    Returns:
-        str: Column letter containing data for dictionary keys.
-    """
-    try:
-        while True:
-            cl = input("\n What Column Letter contains data to use as Keys: ")
-            if cl.isalpha() and not cl.isdigit():
-                return cl.upper()
-    except Exception as e:
-        input(f"\n Error on get_key_col: {e}")
-        exit()
+        except Exception as e:
+            input(f"\n Error on get_header_row: {e}")
+            exit()
 
 
-def get_data_cols() -> list:
-    """
-    Gets a COMMA-SEPARATED list (str) of column letters that contain the 
-    data that will be added as the dictionary values in the data structure.
+    def get_key_col(self) -> object:
+        """
+        Gets column letter containing data to be used as keys in generated dictionary.
+        Adds column letter to .keycol attribute.
 
-    Returns:
-        list: List of uppercase column letters containing needed data.
-    """
-    try:
-        print("\n Enter a COMMA-SEPARATED list of Column Letters to use as values in the dictionary.")
-        datacols = input(" : ")
-        return [x.upper().strip() for x in datacols.split(",")]
-    except Exception as e:
-        input("\n Error on get_data_cols: {e}")
-        exit()
+        Returns:
+            self: PyGen Object.
+        """
+        try:
+            while True:
+                print("\n What Column Letter contains data to use as Keys? ")
+                cl = input(" Enter column letter (Enter 0 to use row numbers): ")
+                if cl == '0':
+                    self.keycol = None
+                    return self
+                if cl.isalpha() and not cl.isdigit():
+                    self.keycol = cl.upper()
+                    return self
+
+        except Exception as e:
+            input(f"\n Error on get_key_col: {e}")
+            exit()
 
 
-def generate_py(gendict, xl) -> None:
-    """
-    Generates "data.py" file containing a dictionary "DATA" of all 
-    information read from the source file. Also adds the date generated 
-    and the source filename as comments up top.
+    def get_data_cols(self) -> object:
+        """
+        Gets a COMMA-SEPARATED list (str) of column letters that contain the 
+        data that will be added as the dictionary values in the data structure.
+        Adds the Upper-Case list to the .dcols attribute.
 
-    Args:
-        gendict (dict): Dictionary of information read from source file.
-        xl (Xlsx): Xlsx object generated from source Excel file.
-    """
-    try:
-        with open(f"{DLS}/data.py", "w") as f:
-            f.write(f"# Generated: {TODAY.month}/{TODAY.day}/{TODAY.year}\n")
-            f.write(f"# Source File: {xl.path.name}\n\n")
-            f.write(f"DATA = {pformat(gendict)}")
-            print('\n "data.py" added to User\'s Downloads folder.')
-    except Exception as e:
-        input("\n Error on generate_py: {e}")
-        exit()
+        Returns:
+            self: PyGen Object.
+        """
+        try:
+            print("\n Enter a COMMA-SEPARATED list of Column Letters to use as values in the dictionary.")
+            datacols = input(" : ")
+            self.dcols = [x.upper().strip() for x in datacols.split(",")]
+            return self
+
+        except Exception as e:
+            input("\n Error on get_data_cols: {e}")
+            exit()
+
+    def get_dict(self) -> object:
+        """
+        Uses Xlsx generate_dictionary method to generate a dictionary. 
+        Adds the dictionary to .gendict attribute.
+
+        Returns:
+            self: PyGen object.
+        """
+        self.gendict = self.generate_dictionary(
+            self.dcols, keycol=self.keycol, hdrrow=self.headerrow)
+        return self
+
+
+    def generate_py(self) -> None:
+        """
+        Generates "data.py" file containing a dictionary "DATA" of all 
+        information read from the source file. Also adds the date generated 
+        and the source filename as comments up top.
+        """
+        try:
+            with open(f"{DLS}/data.py", "w") as f:
+                f.write(f"# Generated: {TODAY.month}/{TODAY.day}/{TODAY.year}\n")
+                f.write(f"# Source File: {self.path.name}\n\n")
+                f.write(f"DATA = {pformat(self.gendict)}")
+                print('\n "data.py" added to User\'s Downloads folder.')
+
+        except Exception as e:
+            input("\n Error on generate_py: {e}")
+            exit()
 
 
 if __name__ == "__main__":
     print('\n Drag and drop your source Excel ("*.xlsx") file below.')
-    xl = Xlsx(dropfile.get())
-    headerrow = get_header_row()
-    keycol = get_key_col()
-    dcols = get_data_cols()
-    gendict = xl.generate_dictionary(
-        keycol, dcols, hdrrow=headerrow, datastartrow=headerrow+1)
-    generate_py(gendict, xl)
+    xl = PyGen(dropfile.get())
+    xl.get_header_row().get_key_col().get_data_cols()
+    xl.get_dict().generate_py()
